50c5fa670e079acbcc7a6bfb3236939b7a5910dd
SurprisingLizard <30316853jay@gmail.com>
2025-12-05 21:24:19 +0800

    簡化與註解

--- main.ino
+++ main.ino
@@ -1,16 +1,21 @@
+/*  
+  此為arduino播放音樂程式
+  引用 Notes.h, sounds 及 durations 檔案
+  以迴圈讀取音高及音長 使用內建函式 tone() 播放
+*/
 #include "Notes.h"
 #include "sounds01.h"
-#include "durations01.h"
 
-int buzzer = 8;
+int buzzer = 8; // 接腳
+int durations = 120; // 節拍數
 
 void setup() {}
 void loop() {
   for (int i = 0; i < sizeof(melody)/sizeof(int); i++) {
-    int duration = 2000 / noteDurations[i];
+    int duration = 240000 / durations / noteDurations[i];
     tone(buzzer, melody[i], duration);
     delay(duration * 1.1);
   }
 
-  delay(2000);
+  delay(5000);
 }
--- turn.py
+++ turn.py
@@ -1,5 +1,11 @@
-import re
+'''
+根據簡譜轉換成音名格式
+其中由於 main 中控制函示需有音高(頻率) 並且一首歌之音符數量眾多
+因此將簡譜轉換結果以逗號分隔輸出 供後續複製於對音高檔案中使用
+'''
 
+import re
+# 簡譜對應音名
 base_notes = {
     '1': ('C', 4),
     '2': ('D', 4),
@@ -11,6 +17,7 @@ base_notes = {
     '0': ('REST', None),
 }
 
+# 升降音對應表
 sharp_map = {
     'C': 'CS',
     'D': 'DS',
@@ -21,19 +28,21 @@ sharp_map = {
     'B': 'B',
 }
 
+# 處理升降音
 def apply_sharp(note, octave):
-    if note == 'REST':
+    if note == 'REST': # 休止符
         return note, octave
 
-    if the_sharp := sharp_map.get(note):
+    if the_sharp := sharp_map.get(note): # 不能升半音 
         if note == 'E':
             return 'F', octave
         if note == 'B':
             return 'C', octave + 1
         return the_sharp, octave
-
+    
     return note, octave
 
+# 簡譜音名轉換
 def convert_token(token):
     m = re.findall(r'(#+)?([0-7])([+]+)?', token)
     if not m:
@@ -41,14 +50,16 @@ def convert_token(token):
 
     results = []
     for sharp_signs, digit, oct_signs in m:
-        base_note, base_oct = base_notes[digit]
+        base_note, base_oct = base_notes[digit] # 一次讀兩個值
 
-        if base_note == 'REST':
+        if base_note == 'REST': # 休止符
             results.append("REST")
             continue
-
+        
+        # 升降八度
         octave = base_oct + (len(oct_signs) if oct_signs else 0)
-
+        
+        # 升降半音
         if sharp_signs:
             for _ in range(len(sharp_signs)):
                 base_note, octave = apply_sharp(base_note, octave)
@@ -57,18 +68,26 @@ def convert_token(token):
 
     return results
 
+# 抓出 token(一個音) 並轉換
 def convert_string(s):
-    tokens = re.findall(r'[#+0-7]+', s)
+    tokens = re.findall(r'[#+0-7]+', s) # 判斷 token
 
     output = []
-    for t in tokens:
+    for t in tokens: # 引用函式轉換
         output.extend(convert_token(t))
     return output
 
+# 整理格式
 def format_single_line(demo):
     return ",".join(convert_string(demo)) + ","
 
-with open("02.txt", "r", encoding="utf-8") as f:
-    content = f.read()
-    
-print(format_single_line(content))
+# 讀取檔案
+def open_file(song):
+    with open(song, "r", encoding="utf-8") as f:
+        content = f.read()
+        return content
+
+# 處理並輸出結果
+song = "01.txt" # 檔案名稱
+muics = format_single_line(open_file(song))
+print(muics)
